ad49bb1c8e9bab9bb77b46fa07796b837ee96cad
unknown <melcec@JAK-MELCEC-LT01.emric.se>
2015-01-29 11:21:39 +0100

    Put share button on menu

--- OMGAndroid/app/src/main/java/com/firstdraftstudio/omgandroid/MainActivity.java
+++ OMGAndroid/app/src/main/java/com/firstdraftstudio/omgandroid/MainActivity.java
@@ -1,39 +1,123 @@
 package com.firstdraftstudio.omgandroid;
 
 import android.app.Activity;
+import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ListView;
+import android.widget.ShareActionProvider;
+import android.widget.TextView;
+
+import java.util.ArrayList;
 
 
-public class MainActivity extends Activity {
+public class MainActivity extends Activity implements View.OnClickListener, AdapterView.OnItemClickListener {
+
+    TextView mainTextView;
+    EditText mainEditText;
+
+    ListView mainListView;
+    ArrayAdapter mArrayAdapter;
+    ArrayList mNameList = new ArrayList();
+
+    ShareActionProvider mShareActionProvider;
+
+    private static final String PREFS = "prefs";
+    private static final String PREF_NAME = "name";
+    SharedPreferences mSharedPreferences;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        //access the textview defined in layout xml
+        //and then set its text
+        mainTextView = (TextView) findViewById(R.id.main_textview);
+        mainTextView.setText("Howdy! Set in Java!");
+
+        Button mainButton;
+        //access the button defined in layout xml
+        //and listen for it here
+        mainButton = (Button) findViewById(R.id.main_button);
+        mainButton.setOnClickListener(this);
+
+        //Access the edittext defined in layout xml
+        mainEditText = (EditText) findViewById(R.id.main_edittext);
+
+        //access the listview
+        mainListView = (ListView) findViewById(R.id.main_listview);
+
+        //create an arrayadapter for the list view
+        mArrayAdapter = new ArrayAdapter(this,
+                android.R.layout.simple_list_item_1,
+                mNameList);
+
+        //set the listview to use the array adapter
+        mainListView.setAdapter(mArrayAdapter);
+
+        //set this activity to react to list items being pressed
+        mainListView.setOnItemClickListener(this);
+
     }
 
+    public void onClick(View v) {
+        //take what was typed into the edittext
+        //and use in textview
+        mainTextView.setText(mainEditText.getText().toString() + "is learning!");
+
+        mNameList.add(mainEditText.getText().toString());
+        mArrayAdapter.notifyDataSetChanged();
+    }
+
+    public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+        Log.d("omg android", position + " : " + mNameList.get(position));
+    }
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
+
+        // Inflate the menu.
+        // Adds items to the action bar if it is present.
         getMenuInflater().inflate(R.menu.menu_main, menu);
+
+        // Access the Share Item defined in menu XML
+        MenuItem shareItem = menu.findItem(R.id.menu_item_share);
+
+        // Access the object responsible for
+        // putting together the sharing submenu
+        if (shareItem != null) {
+            mShareActionProvider = (ShareActionProvider) shareItem.getActionProvider();
+        }
+
+        // Create an Intent to share your content
+        setShareIntent();
+
         return true;
     }
 
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
+    private void setShareIntent() {
 
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
+        if (mShareActionProvider != null) {
+
+            // create an Intent with the contents of the TextView
+            Intent shareIntent = new Intent(Intent.ACTION_SEND);
+            shareIntent.setType("text/plain");
+            shareIntent.putExtra(Intent.EXTRA_SUBJECT, "Android Development");
+            shareIntent.putExtra(Intent.EXTRA_TEXT, mainTextView.getText());
+
+            // Make sure the provider knows
+            // it should work with that Intent
+            mShareActionProvider.setShareIntent(shareIntent);
         }
-
-        return super.onOptionsItemSelected(item);
     }
 }
